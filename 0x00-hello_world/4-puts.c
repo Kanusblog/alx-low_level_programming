93d9463d291b6cbc95f243165f5e034d70473ec7
Kanu Chisom <kanuchisom121@gmail.com>
2022-09-08 09:26:48 -0700

    updated 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Description: prints "Programming is like building a multilingual puzzle"
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *       */
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
